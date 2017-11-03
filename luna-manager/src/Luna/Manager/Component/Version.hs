ff9bff6441936df4f9e5da7f5716be79399592fa
piotrMocz <piotr.moczurad@gmail.com>
2017-11-03 12:37:41 +0100

    Final corrections (really)

--- luna-manager/src/Luna/Manager/Component/Version.hs
+++ luna-manager/src/Luna/Manager/Component/Version.hs
@@ -17,47 +17,56 @@ import Control.Error.Util (hush)
 
 -- === Definition === --
 
-data VersionInfo = Nightly
+data VersionTag = Nightly
                  | Build
                  deriving (Generic, Show, Eq, Ord)
 
+data VersionInfo = VersionInfo { _build :: !Word64
+                               , _tag   :: VersionTag
+                               } deriving (Generic, Show, Eq, Ord)
+
 data Version = Version { _major :: !Word64
                        , _minor :: !Word64
-                       , _build :: !Word64
                        , _info  :: !(Maybe VersionInfo)
                        } deriving (Generic, Show, Eq, Ord)
 
 makeLenses ''Version
 makeLenses ''VersionInfo
 
-nextVersion :: Version -> Version
-nextVersion = (build %~ (+1)) . (info .~ (Just Build))
+cShow = convert . show
 
 -- === Instances === --
 
-instance Pretty VersionInfo where
+instance Pretty VersionTag where
     showPretty Nightly = "nightly"
     showPretty Build   = "build"
     readPretty s = case s of
         "nightly" -> Right Nightly
         "build"   -> Right Build
-        _ -> Left "Incorrect version info format. Expected: \"build\"|\"nightly\""
-        where tryConvert str = mapLeft (const "Conversion error") (tryReads @String str)
+        _ -> Left "Incorrect version tag format. Expected: \"build\"|\"nightly\""
+
+instance Pretty VersionInfo where
+    showPretty (VersionInfo b t) = cShow b <> "-" <> showPretty t
+    readPretty s = case Text.splitOn "-" s of
+        [b, t] -> mapLeft convert $ VersionInfo <$> tryReads b <*> mapLeft convert (readPretty t)
+        _      -> Left "Incorrect version info format. Expected: <build_no>-<version_tag>"
 
 instance Pretty Version where
-    showPretty (Version major minor build info) = intercalate "." (map (convert . show) [major, minor, build])
-                                                <> maybe "" (("-" <>) . showPretty) info
-    readPretty t = case Text.splitOn "." =<< Text.splitOn "-" t of
-        [ma, mi, b, x] -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> tryReads b <*> Right (hush (readPretty x))
-        [ma, mi, b]    -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> tryReads b <*> pure Nothing
-        _              -> Left "Incorrect version format"
+    showPretty (Version ma mi info) = intercalate "." (map (convert . show) [ma, mi])
+                                    <> maybe "" (("." <>) . showPretty) info
+    readPretty t = case Text.splitOn "." t of
+        [ma, mi, i] -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> Right (hush (readPretty i))
+        [ma, mi]    -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> pure Nothing
+        _           -> Left "Incorrect version format"
         where cerr = mapLeft convert
 
 -- JSON
 instance ToJSON      Version     where toEncoding  = JSON.toEncoding . showPretty; toJSON = JSON.toJSON . showPretty
-instance ToJSON      VersionInfo where toEncoding  = lensJSONToEncoding; toJSON = lensJSONToJSON
+instance ToJSON      VersionInfo where toEncoding  = JSON.toEncoding . showPretty; toJSON = JSON.toJSON . showPretty
+instance ToJSON      VersionTag  where toEncoding  = JSON.toEncoding . showPretty; toJSON = JSON.toJSON . showPretty
 instance FromJSON    Version     where parseJSON   = either (fail . convert) return . readPretty <=< parseJSON
-instance FromJSON    VersionInfo where parseJSON   = lensJSONParse
+instance FromJSON    VersionInfo where parseJSON   = either (fail . convert) return . readPretty <=< parseJSON
+instance FromJSON    VersionTag  where parseJSON   = either (fail . convert) return . readPretty <=< parseJSON
 instance FromJSONKey Version     where fromJSONKey = JSON.FromJSONKeyTextParser $ either (fail . convert) return . readPretty
 instance ToJSONKey   Version     where
     toJSONKey = JSON.ToJSONKeyText f g
