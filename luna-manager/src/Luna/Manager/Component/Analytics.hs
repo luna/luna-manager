8a4d3d3958d34203e9e0de0e6e358a327a46481a
piotrMocz <piotr.moczurad@gmail.com>
2017-12-07 16:19:58 +0100

    Correctly looking up versions on Linux + correctly handling missing info

--- luna-manager/src/Luna/Manager/Component/Analytics.hs
+++ luna-manager/src/Luna/Manager/Component/Analytics.hs
@@ -14,7 +14,7 @@ module Luna.Manager.Component.Analytics (
 
 import           Prologue                      hiding ((.=), FilePath)
 
-import           Control.Lens.Aeson            (lensJSONToJSON, lensJSONToEncoding)
+import           Control.Lens.Aeson            (lensJSONToJSON, lensJSONToEncoding, lensJSONParse)
 import           Control.Monad.State.Layered   as SL
 import           Control.Monad.Trans.Resource  (MonadBaseControl)
 import           Data.Aeson                    (FromJSON, ToJSON, toEncoding, toJSON, (.=))
@@ -34,6 +34,7 @@ import qualified Data.UUID.V4                  as UUID
 import           Filesystem.Path.CurrentOS     (FilePath)
 import qualified Filesystem.Path.CurrentOS     as FilePath
 import qualified Network.HTTP.Simple           as HTTP
+import qualified System.IO                     as SIO
 
 import           Luna.Manager.Command.Options  (Options)
 import           Luna.Manager.Component.Pretty (showPretty)
@@ -42,6 +43,7 @@ import           Luna.Manager.Shell.Shelly     (MonadSh, MonadShControl)
 import qualified Luna.Manager.Shell.Shelly     as Shelly
 import           Luna.Manager.System.Host
 import           Luna.Manager.System.Env       (EnvConfig)
+import           Luna.Manager.System.Path      (expand)
 
 default(Text)
 
@@ -91,7 +93,8 @@ instance ToJSON MPUserData where
     toEncoding = lensJSONToEncoding
     toJSON     = lensJSONToJSON
 
-instance FromJSON MPUserData
+instance FromJSON MPUserData where
+    parseJSON = lensJSONParse
 
 instance ToJSON MPUserUpdate where
     toJSON     (MPUserUpdate t d s) = JSON.object ["$token" .= t,   "$distinct_id" .= d,   "$set" .= s]
@@ -112,20 +115,31 @@ newUuid = liftIO $ do
         Just uuid -> return uuid
         Nothing   -> UUID.nextRandom
 
+-- Transforms sth like 'VARNAME="some_text"' into 'some_text'.
+stripVarName :: Text -> Text -> Text
+stripVarName varName txt = fromMaybe "unknown" stripped
+    where stripped = Text.filter (/= '"') <$> (Text.stripPrefix varName $ Text.strip txt)
+
+-- (linux) lookup a var in /etc/*-release files
+lookupSysVar :: (MonadShControl m, MonadSh m) => Text -> m Text
+lookupSysVar varName = do
+    let awkClause = "'/^" <> varName <> "=/'"
+    line <- Shelly.escaping False $ Shelly.cmd "awk" awkClause "/etc/*-release"
+    return $ stripVarName varName line
+
 osVersion :: (MonadShControl m, MonadSh m) => m Text
-osVersion = Shelly.silently $ case currentHost of
+osVersion = do
+    Shelly.silently $ case currentHost of
         Windows ->   Text.strip . (!! 1) . Text.splitOn ":" . head
                  .   filter ("OS Name" `Text.isPrefixOf`) . Text.lines
                  <$> Shelly.cmd "systeminfo"
-        Linux   -> Shelly.cmd "awk" "'/^VERSION=/'" "/etc/*-release" >>=
-                   Shelly.cmd "awk" "-F'='" "'{ print tolower($2) }'"
+        Linux   -> lookupSysVar "VERSION"
         Darwin  -> Text.strip <$> Shelly.cmd "sw_vers" "-productVersion"
 
 osDistro :: (MonadShControl m, MonadSh m) => m Text
 osDistro = Shelly.silently $ case currentHost of
         Windows -> return "N/A"
-        Linux   -> Shelly.cmd "awk" "'/^ID=/'" "/etc/*-release" >>=
-                   Shelly.cmd "awk" "-F'='" "'{ print tolower($2) }'"
+        Linux   -> lookupSysVar "NAME"
         Darwin  -> return "N/A"
 
 -- Gets basic info about the operating system the installer is running on.
@@ -144,16 +158,26 @@ userInfo email = do
                         , _userInfoArch   = currentArch
                         }
 
+strictWrite :: ToJSON s => FilePath -> s -> IO ()
+strictWrite filePath s = do
+    let serialized = toStrict $ JSON.encode s
+        path       = FilePath.encodeString filePath
+    SIO.withFile path SIO.WriteMode $ \h -> do
+        BS.hPut h serialized
+        SIO.hFlush h
+    return ()
+
 -- Checks whether we already have the right user info saved in ~/.luna/user_info.json
 userInfoExists :: (MonadSh m, MonadIO m) => FilePath -> m Bool
 userInfoExists userInfoPath = do
-    fileExists <- Shelly.test_f userInfoPath
+    path <- expand userInfoPath
+    fileExists <- Shelly.test_f path
     if not fileExists then return False
     else do
-        bytes <- liftIO $ BSL.readFile $ FilePath.encodeString userInfoPath
+        bytes <- liftIO $ BSL.readFile $ FilePath.encodeString path
         let userInfoM = JSON.decode bytes :: Maybe MPUserData
             userInfo  = fromMaybe def userInfoM
-        return $ userInfo ^. userInfoUUID == UUID.nil
+        return . not . UUID.null $ userInfo ^. userInfoUUID
 
 -- Saves the email, along with some OS info, to a file user_info.json.
 processUserEmail :: (MonadSh m, MonadShControl m, MonadIO m, MonadBaseControl IO m) =>
@@ -162,7 +186,7 @@ processUserEmail userInfoPath email = do
     info  <- userInfo email
     Shelly.mkdir_p $ FilePath.parent userInfoPath
     Shelly.touchfile userInfoPath
-    liftIO $ BS.writeFile (FilePath.encodeString userInfoPath) (toStrict $ JSON.encode info)
+    liftIO $ strictWrite userInfoPath info
     return info
 
 
@@ -185,8 +209,7 @@ serialize = Base64.encode . toStrict . JSON.encode
 sendMpRequest :: (ToJSON s, MonadIO m, MonadThrow m) => s -> m ()
 sendMpRequest s = do
     let payload = serialize s
-        param   = "data" :: ByteString
-    request <- HTTP.setRequestQueryString [(param, Just payload)] <$>
+    request <- HTTP.setRequestQueryString [("data", Just payload)] <$>
                HTTP.parseRequest userUpdateEndpoint
     liftIO $ void $ HTTP.httpNoBody request
 
@@ -194,7 +217,8 @@ mpRegisterUser :: (MonadIO m, MonadSetter MPUserData m, MonadThrow m,
                    MonadShControl m, MonadSh m, MonadBaseControl IO m) =>
                    FilePath -> Text -> m ()
 mpRegisterUser userInfoPath email = Shelly.unlessM (userInfoExists userInfoPath) $ do
-    userData <- processUserEmail userInfoPath email
+    path     <- expand userInfoPath
+    userData <- processUserEmail path email
     let uuid   = UUID.toText $ userData ^. userInfoUUID
         mpData = MPUserUpdate { _tkn = projectToken
                               , _did = uuid
@@ -202,6 +226,7 @@ mpRegisterUser userInfoPath email = Shelly.unlessM (userInfoExists userInfoPath)
                               }
     put @MPUserData userData
     sendMpRequest mpData
+    return ()
 
 mpTrackEvent :: (MonadIO m, MonadGetters '[MPUserData, Options, EnvConfig] m,
                  MonadThrow m, MonadSh m, MonadShControl m) => Text -> m ()
