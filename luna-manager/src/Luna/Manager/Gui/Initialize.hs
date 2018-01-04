82a4c1819e1a5be24117f85ae097ccf313f25109
piotrMocz <piotr.moczurad@gmail.com>
2018-01-04 14:21:16 +0100

    Hide developer versions in the installer

--- luna-manager/src/Luna/Manager/Gui/Initialize.hs
+++ luna-manager/src/Luna/Manager/Gui/Initialize.hs
@@ -2,17 +2,17 @@
 
 module Luna.Manager.Gui.Initialize where
 
-import Prologue hiding (FilePath)
+import Prologue hiding (FilePath, (.=), (.:))
 import qualified Data.Map as Map
 import Luna.Manager.Component.Repository
 import Luna.Manager.Component.Version
 import Luna.Manager.System.Host
 import Control.Monad.Raise
-import Data.Aeson                    (FromJSON, ToJSON, FromJSONKey, ToJSONKey, parseJSON, encode)
-import qualified Data.Aeson          as JSON
-import qualified Data.Aeson.Types    as JSON
-import qualified Data.Aeson.Encoding as JSON
-import qualified Data.ByteString.Lazy as BS
+import Data.Aeson                         (FromJSON, ToJSON, FromJSONKey, ToJSONKey, parseJSON, encode, (.=), (.:))
+import qualified Data.Aeson               as JSON
+import qualified Data.Aeson.Types         as JSON
+import qualified Data.Aeson.Encoding      as JSON
+import qualified Data.ByteString.Lazy     as BS
 import Control.Lens.Aeson
 
 -- === Definition === --
@@ -48,14 +48,22 @@ instance ToJSON Install
 instance ToJSON Initialize
 instance ToJSON Applications
 instance ToJSON Apps      --  where toEncoding = lensJSONToEncoding; toJSON = lensJSONToJSON
-instance ToJSON Versions
+instance ToJSON Versions where
+    toJSON (Versions d n r) =
+        JSON.object ["release" .= r, "nightly" .= n, ".developer" .= d]
+    toEncoding (Versions d n r) =
+        JSON.pairs  ("release" .= r <> "nightly" .= n <> ".developer" .= d)
 
 instance FromJSON Option
 instance FromJSON Install
-instance FromJSON Initialize   where parseJSON  = lensJSONParse
-instance FromJSON Applications where parseJSON  = lensJSONParse
-instance FromJSON Apps         where parseJSON  = lensJSONParse
-instance FromJSON Versions     where parseJSON  = lensJSONParse
+instance FromJSON Initialize   where parseJSON = lensJSONParse
+instance FromJSON Applications where parseJSON = lensJSONParse
+instance FromJSON Apps         where parseJSON = lensJSONParse
+instance FromJSON Versions where
+    parseJSON = JSON.withObject "Versions" $ \v -> Versions
+        <$> v .: ".developer"
+        <*> v .: "nightly"
+        <*> v .: "release"
 
 resolveAppToInitialize :: (MonadIO m, MonadException SomeException m) => Repo -> Text -> m Apps
 resolveAppToInitialize repo name = do
