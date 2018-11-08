1fcd96f8adbaf368713ede3d22c0a91e17e06e1c
sylwiabr <s.brodacka@gmail.com>
2018-11-08 16:48:27 +0100

    Update luna-manager/src/Luna/Manager/Legal.hs
    Co-Authored-By: mikusp <mikus12@gmail.com>

--- luna-manager/src/Luna/Manager/Legal.hs
+++ luna-manager/src/Luna/Manager/Legal.hs
@@ -6,5 +6,6 @@ companyName, copyright, productName, productDescription
     :: (IsString a, Semigroup a) => a
 companyName        = "New Byte Order Sp. z o. o."
 copyright          = "Copyright (c) 2018 " <> companyName
+-- [TODO]: We should try to extract the `productName` and `productDescription` from application name
 productName        = "Luna Studio"
 productDescription = "Luna Studio"
